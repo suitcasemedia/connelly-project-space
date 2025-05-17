05237dda27ab3980e967210d3f1122ad972a09fc
jimmy <jimmyjconnelly@gmail.com>
2025-05-17 14:52:11 +0100

    added blog to site

---
+++ .eleventy.js
@@ -0,0 +1,45 @@
+const { DateTime } = require("luxon");
+const slugify = require("slugify");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("style.css");
+
+  // ✅ Add date filter
+  eleventyConfig.addFilter("date", dateObj => {
+    return DateTime.fromJSDate(dateObj).toFormat("MMMM d, yyyy");
+  });
+
+  // ✅ Add slug filter
+  eleventyConfig.addFilter("slug", function(input) {
+    return slugify(input, { lower: true, remove: /[*+~.()'"!:@]/g });
+  });
+
+  // ✅ Categories collection
+  eleventyConfig.addCollection("categories", function(collectionApi) {
+    const categoriesSet = new Set();
+    collectionApi.getAll().forEach(item => {
+      const cats = item.data.categories;
+      if (cats) {
+        (Array.isArray(cats) ? cats : [cats]).forEach(c => categoriesSet.add(c));
+      }
+    });
+    return [...categoriesSet];
+  });
+
+  // ✅ Archives collection
+  eleventyConfig.addCollection("archives", function(collectionApi) {
+    const years = new Set();
+    collectionApi.getAllSorted().forEach(item => {
+      if (item.date) years.add(item.date.getFullYear());
+    });
+    return [...years].sort().reverse();
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
---
+++ blog/hello-world.md
@@ -0,0 +1,15 @@
+---
+title: Hello Chickens
+date: 2025-05-16
+layout: layout.njk
+tags: post
+categories:
+  - Chickens
+  - Process
+---
+
+
+This is the very first blog post on the Institute of Chicken Art website.
+
+
+
